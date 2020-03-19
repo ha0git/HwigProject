c92c037f79696f595a18fe350882789267cf92ef
ha0git <lhy7574@naver.com>
2020-03-19 22:14:42 +0900

    0319 조금씩 수정함

--- hwigproject-fe/src/MainComponents/Header.js
+++ hwigproject-fe/src/MainComponents/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Header.css";
 import logo from "../images/logo.svg";
 import hd_down from "../images/hd_down.webp";
+import peng from "../images/peng.jpg";
 
 export default function Header() {
     return (
@@ -11,9 +12,9 @@ export default function Header() {
                     <div className="hd_up">
                         <div className="hd_event"><p>신규 회원가입 시 <span>15% </span>쿠폰 증정</p></div>
                         <ui className="hd_menu">
-                            <li><a href="javascript:void(0);">회원가입</a></li>
-                            <li><a href="javascript:void(0);">로그인</a></li>
-                            <li><a href="javascript:void(0);">고객센터 <img src={hd_down}></img></a>
+                            <li><a href="#">회원가입</a></li>
+                            <li><a href="#">로그인</a></li>
+                            <li><a href="#">고객센터 <img src={hd_down}></img></a>
                                 <ul className="hd_downMenu">
                                     <li><a href="#">공지사항</a></li>
                                     <li><a href="#">자주하는 질문</a></li>
@@ -22,14 +23,15 @@ export default function Header() {
                             </li>
                         </ui>
                     </div>
-                    <div className="hd_logo"><a href="javascript:void(0);">
+                    <div className="hd_logo"><a href="#">
                         <img src={logo}></img>
                     </a>
                     </div>
                     <div class="floating">
-                        <div> 다음뷰 추천위젯코드 삽입</div>
-                        <div>트위터 공유코드 삽입</div>
-                        <div> 페이스북 공유코드 삽입</div>
+                        <div><a href="#"><img src={peng}></img></a></div>
+                        <div><a>장바구니</a></div>
+                        <div><a>등급별 혜택</a></div>
+                        <div><a>최근 본 상품</a></div>
                     </div>
                 </div>
             </div>
--- hwigproject-fe/src/MainComponents/Navi.js
+++ hwigproject-fe/src/MainComponents/Navi.js
@@ -1,26 +1,31 @@
 import React from 'react'
-import {FormControl} from 'react-bootstrap'
+import { FormControl } from 'react-bootstrap'
 import './Navi.css'
+import list from "../images/list.svg"
+import nvBtn from "../images/nvBtn.webp"
 
 export default function Navi() {
     return (
         <>
             <div className="nav-container">
-            <ul className="nav-menu">
-                    <li><a className="nav-category" href="#">전체 카테고리</a>
+                <ul className="nav-menu">
+                    <li><a className="nav-category" href="#"><img src={list}></img>전체 카테고리</a>
                         <ul>
                             <li><a href="#" className="documents">채소</a></li>
-                            <li><a href="#" className="messages">과일 견과 쌀</a></li>
-                            <li><a href="#" className="signout">수산 해산 건어물</a></li>
-                            <li><a href="#" className="signout">정육 계란</a></li>
-                            <li><a href="#" className="signout">국 반찬 메인요리</a></li>
+                            <li><a href="#" className="messages">과일 · 견과 · 쌀</a></li>
+                            <li><a href="#" className="signout">수산 · 해산 · 건어물</a></li>
+                            <li><a href="#" className="signout">정육 · 계란</a></li>
+                            <li><a href="#" className="signout">국 · 반찬 · 메인요리</a></li>
                         </ul>
                     </li>
                     <li><a className="nav-items" href="#">신상품</a></li>
                     <li><a className="nav-items" href="#">베스트</a></li>
                     <li><a className="nav-items" href="#">알뜰쇼핑</a></li>
                     <li><a className="nav-items" href="#">이벤트</a></li>
-                    <li className="nav-search-container"><FormControl id="nav-search" type="text" placeholder="Search" className="mr-sm-2"/></li>
+                    <li className="nav-search-container">
+                        <FormControl id="nav-search" type="text" placeholder="Search" className="mr-sm-2" />
+                        <input type="image" className="nav-btn" src={nvBtn}></input>
+                    </li>
                 </ul>
             </div>
         </>
