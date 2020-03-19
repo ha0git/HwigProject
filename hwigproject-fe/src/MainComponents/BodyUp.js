01d6c0cbeab44a89027cbd0f5ef5b50b78c61d9b
병철아 밥한번 먹자 <nomoretomatojuice@nomoretomatojuice.local>
2020-03-19 16:51:51 +0900

    body update

--- hwigproject-fe/src/MainComponents/BodyUp.js
+++ hwigproject-fe/src/MainComponents/BodyUp.js
@@ -12,38 +12,38 @@ export default function BodyUp() {
                         <div>
                             <ul className="main_goods_ul">
                                 <li className="main_goods_li">
-                                    <a><img className="main_thumb_img" src="https://img-cf.kurly.com/shop/data/goods/1583815961374l0.jpg" /></a>
+                                    <a href="#"><img className="main_thumb_img" src="https://img-cf.kurly.com/shop/data/goods/1583815961374l0.jpg" /></a>
                                     <div className="info_goods">
                                         <span className="goods_name">
-                                            <a className="nametxt">고소한 비엔나 소세지</a>
-                                        </span>
+                                            <a href="#" className="nametxt">고소한 비엔나 소세지</a>
+                                        </span><p />
                                         <span className="price">38000원</span>
                                     </div>
                                 </li>
                                 <li className="main_goods_li">
-                                    <a><img className="main_thumb_img" src="https://img-cf.kurly.com/shop/data/goods/1583815961374l0.jpg" /></a>
+                                    <a href="#"><img className="main_thumb_img" src="https://img-cf.kurly.com/shop/data/goods/1583815961374l0.jpg" /></a>
                                     <div className="info_goods">
                                         <span className="goods_name">
-                                            <a className="nametxt">고소한 비엔나 소세지</a>
-                                        </span>
+                                            <a href="#" className="nametxt">고소한 비엔나 소세지</a>
+                                        </span><p />
                                         <span className="price">38000원</span>
                                     </div>
                                 </li>
                                 <li className="main_goods_li">
-                                    <a><img className="main_thumb_img" src="https://img-cf.kurly.com/shop/data/goods/1583815961374l0.jpg" /></a>
+                                    <a href="#"><img className="main_thumb_img" src="https://img-cf.kurly.com/shop/data/goods/1583815961374l0.jpg" /></a>
                                     <div className="info_goods">
                                         <span className="goods_name">
-                                            <a className="nametxt">고소한 비엔나 소세지</a>
-                                        </span>
+                                            <a href="#" className="nametxt">고소한 비엔나 소세지</a>
+                                        </span><p />
                                         <span className="price">38000원</span>
                                     </div>
                                 </li>
                                 <li className="main_goods_li">
-                                    <a><img className="main_thumb_img" src="https://img-cf.kurly.com/shop/data/goods/1583815961374l0.jpg" /></a>
+                                    <a href="#"><img className="main_thumb_img" src="https://img-cf.kurly.com/shop/data/goods/1583815961374l0.jpg" /></a>
                                     <div className="info_goods">
                                         <span className="goods_name">
-                                            <a className="nametxt">고소한 비엔나 소세지</a>
-                                        </span>
+                                            <a href="#" className="nametxt">고소한 비엔나 소세지</a>
+                                        </span><p />
                                         <span className="price">38000원</span>
                                     </div>
                                 </li>
@@ -52,6 +52,51 @@ export default function BodyUp() {
                     </div>
                 </div>
                 
+                <div className="main_product_list">
+                    <div className="main_goods_tit_d"><a href="#"><h3 className="main_goods_tit">알뜰 상품</h3></a></div>
+                    <div className="main_list_goods">
+                        <div>
+                            <ul className="main_goods_ul">
+                                <li className="main_goods_li">
+                                    <a href="#"><img className="main_thumb_img" src="https://img-cf.kurly.com/shop/data/goods/1583815961374l0.jpg" /></a>
+                                    <div className="info_goods">
+                                        <span className="goods_name">
+                                            <a href="#" className="nametxt">고소한 비엔나 소세지</a>
+                                        </span><p />
+                                        <span className="price">38000원</span>
+                                    </div>
+                                </li>
+                                <li className="main_goods_li">
+                                    <a href="#"><img className="main_thumb_img" src="https://img-cf.kurly.com/shop/data/goods/1583815961374l0.jpg" /></a>
+                                    <div className="info_goods">
+                                        <span className="goods_name">
+                                            <a href="#" className="nametxt">고소한 비엔나 소세지</a>
+                                        </span><p />
+                                        <span className="price">38000원</span>
+                                    </div>
+                                </li>
+                                <li className="main_goods_li">
+                                    <a href="#"><img className="main_thumb_img" src="https://img-cf.kurly.com/shop/data/goods/1583815961374l0.jpg" /></a>
+                                    <div className="info_goods">
+                                        <span className="goods_name">
+                                            <a href="#" className="nametxt">고소한 비엔나 소세지</a>
+                                        </span><p />
+                                        <span className="price">38000원</span>
+                                    </div>
+                                </li>
+                                <li className="main_goods_li">
+                                    <a href="#"><img className="main_thumb_img" src="https://img-cf.kurly.com/shop/data/goods/1583815961374l0.jpg" /></a>
+                                    <div className="info_goods">
+                                        <span className="goods_name">
+                                            <a href="#" className="nametxt">고소한 비엔나 소세지</a>
+                                        </span><p />
+                                        <span className="price">38000원</span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
