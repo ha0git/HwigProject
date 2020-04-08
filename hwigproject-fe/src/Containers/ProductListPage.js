dd12d0488e338ef7e1087d04c74d651bf5059275
ha0git <lhy7574@naver.com>
2020-04-08 10:24:39 +0900

    0408 productlist update

--- hwigproject-fe/src/Containers/ProductListPage.js
+++ hwigproject-fe/src/Containers/ProductListPage.js
@@ -1,15 +1,130 @@
 import React, { useState, useEffect } from 'react'
 import { Spinner } from 'react-bootstrap'
+import queryString from 'query-string'
 import ProductList from '../ProductComponents/ProductList'
-import product_img from '../images/product1.png'
+import product1 from '../images/product1.png'
 
-export default function ProductListPage() {
-    const [product, setProduct] = useState("");
-    const [categoriy, setCategory] = useState("");
+export default function ProductListPage({ history, location }) {
+    const [productItems, setProductItems] = useState(null)
 
+    const query = queryString.parse(location.search);
+    const [page, setPage] = useState(1)
+    const [size, setSize] = useState(9)
+
+    useEffect(() => {
+        if (query.page === undefined) {
+            history.push(`/shop?page=${page}`)
+        }
+        if (query.page !== page) {
+            setPage(parseInt(query.page));
+            console.log(page)
+        }
+        if (productItems === null) {
+            setProductItems({
+                category: [
+                    {
+                        category_id: 0,
+                        category_name: "전체 보기"
+                    },
+                    {
+                        category_id: 1,
+                        category_name: "기본 채소"
+                    },
+                    {
+                        category_id: 2,
+                        category_name: "국산 과일"
+                    },
+                    {
+                        category_id: 3,
+                        category_name: "수입 과일"
+                    }
+                ],
+                product: [
+                    {
+                        id: 0,
+                        prd_img: product1,
+                        prd_name: '무농약 통셀러리 500g',
+                        prd_price: '3,510원',
+                        prd_comment: '셀러리 맛없어 사지마 먹지마'
+                    },
+                    {
+                        id: 1,
+                        prd_img: product1,
+                        prd_name: '냉동 바밤 단호박 250g',
+                        prd_price: '2,950원',
+                        prd_comment: '손질없이 즐기는 간편 단호박'
+                    },
+                    {
+                        id: 2,
+                        prd_img: product1,
+                        prd_name: '무농약 통셀러리 500g',
+                        prd_price: '3,510원',
+                        prd_comment: '셀러리 맛없어 사지마 먹지마'
+                    },
+                    {
+                        id: 3,
+                        prd_img: product1,
+                        prd_name: '무농약 통셀러리 500g',
+                        prd_price: '3,510원',
+                        prd_comment: '셀러리 맛없어 사지마 먹지마'
+                    },
+                    {
+                        id: 4,
+                        prd_img: product1,
+                        prd_name: '무농약 통셀러리 500g',
+                        prd_price: '3,510원',
+                        prd_comment: '셀러리 맛없어 사지마 먹지마'
+                    },
+                    {
+                        id: 5,
+                        prd_img: product1,
+                        prd_name: '무농약 통셀러리 500g',
+                        prd_price: '3,510원',
+                        prd_comment: '셀러리 맛없어 사지마 먹지마'
+                    },
+                    {
+                        id: 6,
+                        prd_img: product1,
+                        prd_name: '무농약 통셀러리 500g',
+                        prd_price: '3,510원',
+                        prd_comment: '셀러리 맛없어 사지마 먹지마'
+                    },
+                    {
+                        id: 7,
+                        prd_img: product1,
+                        prd_name: '무농약 통셀러리 500g',
+                        prd_price: '3,510원',
+                        prd_comment: '셀러리 맛없어 사지마 먹지마'
+                    },
+                    {
+                        id: 8,
+                        prd_img: product1,
+                        prd_name: '무농약 통셀러리 500g',
+                        prd_price: '3,510원',
+                        prd_comment: '셀러리 맛없어 사지마 먹지마'
+                    },
+                    {
+                        id: 9,
+                        prd_img: product1,
+                        prd_name: '무농약 통셀러리 500g',
+                        prd_price: '3,510원',
+                        prd_comment: '셀러리 맛없어 사지마 먹지마'
+                    },
+                    {
+                        id: 10,
+                        prd_img: product1,
+                        prd_name: '무농약 통셀러리 500g',
+                        prd_price: '3,510원',
+                        prd_comment: '셀러리 맛없어 사지마 먹지마'
+                    },
+                ]
+
+            })
+        }
+    })
     return (
         <>
-
+            {productItems && <ProductList productItems={productItems} page={page} size={size} history={history} />}
         </>
     )
 }
--- hwigproject-fe/src/ProductComponents/ProductList.js
+++ hwigproject-fe/src/ProductComponents/ProductList.js
@@ -1,134 +1,79 @@
-import React, { useState } from 'react'
-import { Image } from 'react-bootstrap'
-// import jQuery from "jquery";
+import React, { useState, Children } from 'react'
 import { Link } from 'react-router-dom'
 import Pagination from "react-js-pagination";
+import ImageMapper from 'react-image-mapper';
+// import jQuery from "jquery";
 import './ProductList.css'
 
 export default function ProductList(props) {
-    const productList = props.productItems.product
     const categories = props.productItems.category
+    const productList = props.productItems.product
+
+    const categoryList = categories.map(category => (
+        <li key={category.id}><Link to="/">{category.category_name}</Link></li>))
+
+    const productGoods = productList.map(product => (
+        <li key={product.id}>
+            <Link><ImageMapper src={product.prd_img}></ImageMapper></Link>
+            <Link>
+                <span className="product_list_goods_name">{product.prd_name}</span>
+                <span className="product_list_goods_price">{product.prd_price}</span>
+                <span className="product_list_goods_desc">{product.prd_comment}</span>
+            </Link>
+        </li>
+    ))
+
+    const showPrdList = () => {
+        let list = [];
+        let begin = (props.page - 1) * props.size;
+        let end;
+        if (productList.length < props.page * 9) {
+            end = productList.length
+        } else {
+            end = props.page * 9;
+        }
+        console.log(begin, end)
+
+        for (let i = begin; i < end; i++) {
+            list.push(productGoods[i])
+        }
+        console.log(list)
+        return list
+    }
+
     const [activePage, setactivePage] = useState(15);
 
     const handlePageChange = (pageNumber) => {
         console.log(`active page is ${pageNumber}`);
         setactivePage(pageNumber)
+        props.history.push(`/shop?page=${pageNumber}`)
     }
-
 
     return (
         <>
-            <div className="product-list-body">
-                <div>
-                    <p className="product-title"></p>
-                </div>
-                <nav className="product-category-container">
-                    <span>
-                        <Link className="product-category" to="/category">
-                            <span className="product-category-button">{categories[0].name}</span>
-                        </Link>
-                    </span>
-                    <span>
-                        <Link className="product-category" to="/category">
-                            <span className="product-category-button">{categories[1].name}</span>
-                        </Link>
-                    </span>
-                    <span>
-                        <Link className="product-category" to="/category">
-                            <span className="product-category-button">{categories[2].name}</span>
-                        </Link>
-                    </span>
-                    <hr />
-                </nav>
-                <div className="product-list-content">
-                    <div className="product">
-                        <Link className="product-list-img" to="/">
-                            <Image className="product-list-img" src={productList[0].img} rounded />
-                        </Link>
-                        <div className="product-text-title">
-                            <Link className="producet-text-link" to="/">{productList[0].title}</Link>
+            <div className="product_list">
+                <div className="product_list_content">
+                    <div className="product_list_header">
+                        <div className="product_list_tit">
+                            <span>채소&nbsp;·&nbsp;과일</span>
                         </div>
-                        <div className="product-text-price">{productList[0].price}</div>
+                        <ul className="product_list_category">
+                            {categoryList}
+                        </ul>
                     </div>
-                    <div className="product">
-                        <Link className="product-list-img" to="/">
-                            <Image className="product-list-img" src={productList[0].img} rounded />
-                        </Link>
-                        <div className="product-text-title">
-                            <Link className="producet-text-link" to="/">{productList[0].title}</Link>
-                        </div>
-                        <div className="product-text-price">{productList[0].price}</div>
-                    </div>
-                    <div className="product">
-                        <Link className="product-list-img" to="/">
-                            <Image className="product-list-img" src={productList[0].img} rounded />
-                        </Link>
-                        <div className="product-text-title">
-                            <Link className="producet-text-link" to="/">{productList[0].title}</Link>
-                        </div>
-                        <div className="product-text-price">{productList[0].price}</div>
-                    </div>
-                    <div className="product">
-                        <Link className="product-list-img" to="/">
-                            <Image className="product-list-img" src={productList[0].img} rounded />
-                        </Link>
-                        <div className="product-text-title">
-                            <Link className="producet-text-link" to="/">{productList[0].title}</Link>
-                        </div>
-                        <div className="product-text-price">{productList[0].price}</div>
-                    </div>
-                    <div className="product">
-                        <Link className="product-list-img" to="/">
-                            <Image className="product-list-img" src={productList[0].img} rounded />
-                        </Link>
-                        <div className="product-text-title">
-                            <Link className="producet-text-link" to="/">{productList[0].title}</Link>
-                        </div>
-                        <div className="product-text-price">{productList[0].price}</div>
-                    </div>
-                    <div className="product">
-                        <Link className="product-list-img" to="/">
-                            <Image className="product-list-img" src={productList[0].img} rounded />
-                        </Link>
-                        <div className="product-text-title">
-                            <Link className="producet-text-link" to="/">{productList[0].title}</Link>
-                        </div>
-                        <div className="product-text-price">{productList[0].price}</div>
-                    </div>
-                    <div className="product">
-                        <Link className="product-list-img" to="/">
-                            <Image className="product-list-img" src={productList[0].img} rounded />
-                        </Link>
-                        <div className="product-text-title">
-                            <Link className="producet-text-link" to="/">{productList[0].title}</Link>
-                        </div>
-                        <div className="product-text-price">{productList[0].price}</div>
-                    </div>
-                    <div className="product">
-                        <Link className="product-list-img" to="/">
-                            <Image className="product-list-img" src={productList[0].img} rounded />
-                        </Link>
-                        <div className="product-text-title">
-                            <Link className="producet-text-link" to="/">{productList[0].title}</Link>
-                        </div>
-                        <div className="product-text-price">{productList[0].price}</div>
-                    </div>
-                    <div className="product">
-                        <Link className="product-list-img" to="/">
-                            <Image className="product-list-img" src={productList[0].img} rounded />
-                        </Link>
-                        <div className="product-text-title">
-                            <Link className="producet-text-link" to="/">{productList[0].title}</Link>
-                        </div>
-                        <div className="product-text-price">{productList[0].price}</div>
+                    <div className="product_list_body">
+                        <ul className="product_list_goods">
+                            {showPrdList()}
+                        </ul>
+
                     </div>
                 </div>
                 <div className="product-pagenation-container">
                     <Pagination
                         activePage={activePage}
-                        itemsCountPerPage={10}
-                        totalItemsCount={100}
-                        pageRangeDisplayed={5}
+                        itemsCountPerPage={9}
+                        totalItemsCount={productList.length}
+                        pageRangeDisplayed={9}
                         onChange={handlePageChange}
                     />
                 </div>
