b3240c601da1a68c12877abd0bdb495f13f29aef
병철아 밥한번 먹자 <ssar509@gmail.com>
2020-04-16 17:28:33 +0900

    0416 injune

--- hwig_front/src/ProductComponents/Cart.js
+++ hwig_front/src/ProductComponents/Cart.js
@@ -1,9 +1,67 @@
-import React from 'react'
-import {Col, Row, Table, Form, Button} from 'react-bootstrap'
+import React, { useState, useEffect }  from 'react'
+import {Table} from 'react-bootstrap'
 import './Cart.css'
 import vienna from '../images/product/vienna.png';
 
-export default function Cart() {
+export default function Cart(props) {
+    const goodsInfo = props.prdList.product
+    const rise = (quantities) => {
+        quantities = quantities + 1
+    }
+    const reduce = (quantities) => {
+        quantities = quantities - 1
+
+    }
+
+    
+    const productGoods = goodsInfo.map(goodsInfo => {
+
+        const quantity = goodsInfo.prd_quantity
+        
+        return (
+        <tbody>
+                <tr>
+                    {/*<td className="cart-table-item-none" colspan="5"><br/><br/>장바구니에 담긴 상품이 없습니다.<br/><br/><br/></td>*/}
+                    <td>
+                        <label className="ch">
+                        <input type="checkbox" id="cart_check2"></input>
+                        <span></span>
+                    </label>
+                    </td>
+                    <td>
+                        <div className="cart-goods-thumb">
+                            <img src={goodsInfo.prd_thumb_img} />
+                        </div>
+                        <div className="cart-goods-desc">
+                            <div className="cart-goods-name">
+                            {goodsInfo.prd_name}
+                            </div>
+                            <br />
+                            <div className="cart-goods-price">
+                            {goodsInfo.prd_price}원
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="cart-goods-quantity">
+                            <button type="button" className="btn_reduce" onClick={reduce(quantity)}>
+                                <img src="https://res.kurly.com/pc/ico/1801/ico_minus_24x4_777.png" />
+                            </button> 
+                            <input type="text" className="inp_quantity" value={quantity}/>
+                            <button type="button" className="btn_rise" onClick={rise(quantity)}>
+                                <img src="https://res.kurly.com/pc/ico/1801/ico_plus_24x24_777.png" />
+                            </button>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="cart-goods-price2">
+                            {goodsInfo.prd_price * goodsInfo.prd_quantity}원
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        )
+    } )
     return (
         <>
             <div className="cart-container">
@@ -15,7 +73,7 @@ export default function Cart() {
                                 
                             <form>
                                 
-                                <Table className="table">
+                                <Table className="cart_table">
                                     <thead>
                                         <tr>
                                             <th className="cart-table-column-check">
@@ -28,54 +86,13 @@ export default function Cart() {
                                             <th className="cart-table-column-price"><div className="cartd">상품금액</div></th>
                                         </tr>
                                     </thead>
-                                    <tbody>
-                                        <tr>
-                                            {/*<td className="cart-table-item-none" colspan="5"><br/><br/>장바구니에 담긴 상품이 없습니다.<br/><br/><br/></td>*/}
-                                            <td>
-                                                <label className="ch">
-                                                <input type="checkbox" id="car_check2"></input>
-                                                <span></span>
-                                            </label>
-                                            </td>
-                                            <td>
-                                                <div className="cart-goods-thumb">
-                                                    <img src={vienna} />
-                                                </div>
-                                                <div className="cart-goods-desc">
-                                                    <div className="cart-goods-name">
-                                                    고소한 비엔나 소세지
-                                                    </div>
-                                                    <br />
-                                                    <div className="cart-goods-price">
-                                                    38,000 원
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div className="cart-goods-quantity">
-                                                    <button type="button" className="btn_reduce">
-                                                        <img src="https://res.kurly.com/pc/ico/1801/ico_minus_24x4_777.png" />
-                                                    </button> 
-                                                    <input type="text" className="inp_quantity" /> 
-                                                    <button type="button" className="btn_rise">
-                                                        <img src="https://res.kurly.com/pc/ico/1801/ico_plus_24x24_777.png" />
-                                                    </button>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div className="cart-goods-price2">
-                                                    38,000 원
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                
+                                    {productGoods}
                                 </Table>
                                 <div className="cart-button">
                                 <button className="cart-delete-button">선택상품 삭제</button>
                                 <button className="cart-delete-button">품절상품 삭제</button>
                                 </div>
-                                <Table className="table2">
+                                <Table className="cart_table2">
                                     <thead>
                                         <tr>
                                             <th className="cart-calculate-order"><div className="cartd">총 주문금액</div></th>
