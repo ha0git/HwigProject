726e16eb02d3367e0afe7d38b18a40589ec63b96
ha0git <lhy7574@naver.com>
2020-04-20 22:17:38 +0900

    0420 navi axios success

--- hwig_front/src/Containers/ServerAddress.js
+++ hwig_front/src/Containers/ServerAddress.js
@@ -1,3 +1,4 @@
 // export const host = "http://localhost:8181/cds/"
-export const host = "http://192.168.0.206:8080/"
+//export const host = "http://192.168.0.206:8080/"
 // export const host = "http://localhost:3000"
+export const host = "http://192.168.0.233:8090/"
