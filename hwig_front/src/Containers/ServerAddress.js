e93abbcb5fb9931301dbf17ef9d9966dc4ffce06
ha0git <lhy7574@naver.com>
2020-05-17 21:03:48 +0900

    0517 orderUpdate

--- hwig_front/src/Containers/ServerAddress.js
+++ hwig_front/src/Containers/ServerAddress.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-//export const host = "http://13.209.202.242:8080/"
+export const host = "http://13.209.202.242:8080/"
 //export const host = "http://192.168.0.206:8080/"
 
 //export const host = "http://192.168.0.233:8090/"
 
-//export const host = "http://50b4671d.ngrok.io/"
+//export const host = "http://e30e848e.ngrok.io/"
 
-=======
-export const host = "http://13.209.202.242:8080/"
->>>>>>> f22ffee1fde903bb0fc0a8912b57a6ecb5640a1c
